c81b810b3314c16c4541a18be71bd8520a3eadd5
Nataliia <nkopichinckaya@gmail.com>
2025-03-07 11:28:17 +0100

    add all

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ export function setupServer() {
   });
   app.get('/contacts', async (req, res) => {
     try {
-      const contacts = await Contact.find();
+      const contacts = await getAllContacts();
       res.json({
         status: 200,
         message: 'Successfully found contacts!',
@@ -39,13 +39,17 @@ export function setupServer() {
   app.get('/contacts/:contactId', async (req, res) => {
     try {
       const { contactId } = req.params;
-      const contact = await Contact.findById(contactId);
+      const contact = await getContactById(contactId);
       if (contact === null) {
         return res.status(404).json({
           message: 'Contact not found',
         });
       }
-      res.json(contact);
+      res.json({
+        status: 200,
+        message: 'Successfully found contact with id {contactId}!',
+        data: contact,
+      });
     } catch (error) {
       res.status(500).json({
         status: 500,
