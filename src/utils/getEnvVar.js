c02de86af970a931bab392d277e65659ee7e79e8
Nataliia <nkopichinckaya@gmail.com>
2025-04-02 13:00:54 +0200

    add 6

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,10 +1,17 @@
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-export function getEnvVar(name, defaultValue) {
+// export  function getEnvVar(name, defaultValue) {
+//   const value = process.env[name];
+//   if (value) return value;
+//   if (defaultValue) return defaultValue;
+
+//   throw new Error(`missing process.env.${name}`);
+// }
+
+export function getEnvVar(name) {
   const value = process.env[name];
   if (value) return value;
-  if (defaultValue) return defaultValue;
 
   throw new Error(`missing process.env.${name}`);
 }
