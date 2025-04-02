9b0969090098a69444c507349f39cbfffe7170f5
Nataliia <nkopichinckaya@gmail.com>
2025-04-02 11:44:09 +0200

    add 6

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,12 +1,14 @@
 import cloudinary from 'cloudinary';
-import getEnvVar from '../utils/getEnvVar.js';
+import getEnvVar from './getEnvVar.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: getEnvVar('CLOUDINARY_CLOUD_NAME'),
-  //   cloud_name: 'dokj7aq0x',
-  api_key: getEnvVar('CLOUDINARY_API_KEY'),
-  api_secret: getEnvVar('CLOUDINARY_API_SECRET'),
+  //   cloud_name: getEnvVar('CLOUDINARY_CLOUD_NAME'),
+  cloud_name: 'dokj7aq0x',
+  api_key: '862559877363528',
+  api_secret: 'VUPoo6ro5NHYfxZer2pvybxBSlY',
+  //   api_key: getEnvVar('CLOUDINARY_API_KEY'),
+  //   api_secret: getEnvVar('CLOUDINARY_API_SECRET'),
 });
 
 export function uploadToCloudinary(filePath) {
